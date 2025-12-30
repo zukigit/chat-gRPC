375bba00ab9188d75f92d9245a8f0e4784ad73b2
zukidono <zukidono@gmail.com>
2025-12-30 22:55:57 +0700

    added jwt token

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zukigit/chat-gRPC
 go 1.25.5
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.3.0
 	google.golang.org/grpc v1.78.0
 	google.golang.org/protobuf v1.36.11
 )
--- src/server/main.go
+++ src/server/main.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/zukigit/chat-gRPC/src/protos/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -15,22 +20,65 @@ const (
 )
 
 type server struct {
+	secretKey []byte
+	mu        sync.RWMutex
+	userStore map[string]string
 	auth.UnimplementedAuthServer
 }
 
+func (s *server) register(userName, passwd string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.userStore[userName] = passwd
+}
+
+func (s *server) generateToken(userName string) (string, error) {
+	claims := &jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		Issuer:    "auth",
+		Subject:   userName,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(s.secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
 func (s *server) Login(ctx context.Context, request *auth.LoginRequest) (*auth.LoginResponse, error) {
-	fmt.Println("username", request.GetUserName())
-	fmt.Println("passwd", request.GetPasswd())
+	passwd, exists := s.userStore[request.GetUserName()]
+	if !exists {
+		s.register(request.GetUserName(), request.GetPasswd())
+		goto GENERATE_TOKEN
+	}
+
+	if passwd != request.GetPasswd() {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid username or password")
+	}
+
+GENERATE_TOKEN:
+	token, err := s.generateToken(request.GetUserName())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "could not generate token")
+	}
 
 	return &auth.LoginResponse{
-		Token: "123123",
+		Token: token,
 	}, nil
 }
 
 func main() {
 	fmt.Println("starting server")
 
-	srv := &server{}
+	srv := &server{
+		userStore: make(map[string]string),
+		secretKey: []byte("no_secret"),
+	}
 
 	s := grpc.NewServer()
 	auth.RegisterAuthServer(s, srv)
