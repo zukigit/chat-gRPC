9855ab0495f5a882167217cadb244b5c0dc4efff
zukidono <zukidono@gmail.com>
2025-12-30 15:53:09 +0700

    init server

--- src/server/main.go
+++ src/server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/zukigit/chat-gRPC/src/protos/auth"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = ":56789"
+)
+
+type server struct {
+	auth.UnimplementedAuthServer
+}
+
+func (s *server) Login(ctx context.Context, request *auth.LoginRequest) (*auth.LoginResponse, error) {
+	fmt.Println("username", request.GetUserName())
+	fmt.Println("passwd", request.GetPasswd())
+
+	return &auth.LoginResponse{
+		Token: "123123",
+	}, nil
+}
 
 func main() {
-	fmt.Println("it is server")
+	fmt.Println("starting server")
+
+	srv := &server{}
+
+	s := grpc.NewServer()
+	auth.RegisterAuthServer(s, srv)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("Server listening at %v", listener.Addr())
+
+	s.Serve(listener)
 }
